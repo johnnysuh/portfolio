b1b2b3b435520becd1d96c36be4d0621b15a2894
johnnysuh <nicolas_c_duarte@estudante.sesisenai.org.br>
2023-11-12 19:43:51 -0300

    commit

--- inverter-string.js
+++
@@ -1,8 +0,0 @@
-function reverseString(str) {
-    var newString
-    for (var i = str.lenght - 1; i > 0; i--) {
-    newString += str[i];
-    }
-    return newString;
-}
-reverseString("Javascript")
